acdc3324bce5cb543bffa30f533a4e4a29f3e519
cyberrunk <1615881531@qq.com>
2020-11-16 23:35:38 +0800

    merge sort

---
+++ merge_sort.py
@@ -0,0 +1,27 @@
+def merge_sort(l):
+    if len(l) < 2:
+        return l
+    else:
+        mid = len(l) // 2
+        left = merge_sort(l[:mid])
+        right = merge_sort(l[mid:])
+        return merge(left, right)
+
+
+def merge(left, right):
+    res = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+    while i < len(left):
+        res.append(left[i])
+        i += 1
+    while j < len(right):
+        res.append(right[j])
+        j += 1
+    return res
